204bfe51a6f7947c98a681a05c0fe2d803f7b04b
Familia <Familia@RodrAvilHP>
2024-09-18 21:15:29 -0500

    FILE HANDLER FULL

--- TallerProgramacion/src/co/edu/unbosque/model/persistence/FileHandler.java
+++ TallerProgramacion/src/co/edu/unbosque/model/persistence/FileHandler.java
@@ -1,5 +1,70 @@
 package co.edu.unbosque.model.persistence;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FileHandler {
+
+	private static Scanner sc;
+	private static PrintWriter pw;
+	private static File archivo;
+	private static final String FOLDER_NAME = "data";
+
+	public static void checkFolder() {
+
+		archivo = new File(FOLDER_NAME);
+		if (archivo.exists() && archivo.isDirectory()) {
+			return;
+		} else {
+			archivo.mkdir();
+		}
+
+	}
+
+	public static void writeFile(String url, String content) {
+		try {
+			archivo = new File(FOLDER_NAME + "/" + url);
+			if (!archivo.exists()) {
+				archivo.createNewFile();
+			}
+			pw = new PrintWriter(archivo);
+			pw.write(content);
+			pw.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR EN LA ESCRITURA DEL ARCHIVO (PLAIN TEXT)");
+			// e.printStackTrace();
+		} catch (IOException e) {
+
+			System.out.println("ERROR EN LA CREACION DEL ARCHIVO (PLAIN TEXT)");
+			// e.printStackTrace();
+		}
+
+	}
+
+	public static String readFile(String url) {
+
+		try {
+			archivo = new File(FOLDER_NAME + "/" + url);
+			if (!archivo.exists()) {
+				archivo.createNewFile();
+			}
+
+			sc = new Scanner(archivo);
+			String content = "";
+			while (sc.hasNext()) {
+				content += sc.nextLine() + "\n";
+			}
+
+			return content;
+
+		} catch (IOException e) {
+			System.out.println("ERROR EN LA LECTURA DEL ARCHIVO (PLAIN TEXT)");
+		}
+
+		return null;
+	}
 
 }
